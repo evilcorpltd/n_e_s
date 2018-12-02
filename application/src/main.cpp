7603bdc7f1e76d2692d8ac94b26dc1a073564409
Mikael Larsson <c.mikael.larsson@gmail.com>
2018-12-02 21:53:17 +0100

    Fixed creation of mmu in main

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2018 Robin Linden <dev@robinlinden.eu>
 
 #include "core/cpu_factory.h"
+#include "core/membank_factory.h"
 #include "core/mmu_factory.h"
 #include "core/ppu_factory.h"
 
@@ -9,7 +10,8 @@
 using namespace n_e_s::core;
 
 int main(int, char **) {
-    std::unique_ptr<IMmu> mmu{MmuFactory::create()};
+    MemBankList mem_banks(MemBankFactory::create_nes_mem_banks());
+    std::unique_ptr<IMmu> mmu{MmuFactory::create(std::move(mem_banks))};
 
     Registers registers;
     std::unique_ptr<ICpu> cpu{CpuFactory::create(&registers, mmu.get())};
