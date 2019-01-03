7fcfa64f70ec0f31cbb31b186c75d8339f041c0a
Robin Lindén <dev@robinlinden.eu>
2019-01-03 16:17:14 +0100

    Remove debug prints

--- core/src/rom_factory.cpp
+++ core/src/rom_factory.cpp
@@ -3,7 +3,6 @@
 #include "rom/nrom.h"
 
 #include <cassert>
-#include <cstdio>
 #include <cstring>
 #include <fstream>
 #include <sstream>
@@ -57,21 +56,8 @@ IRom *RomFactory::fromFile(const std::string &filepath) {
         h.prg_ram_size = 1; // For compatibility reasons, 0 ram means 1 ram.
     }
 
-    // Not null terminated, so we'll get some trash, but that's fine.
-    printf("NES: %s\n", h.nes);
-    printf("prg_rom: %u (%u)\n", h.prg_rom_size, h.prg_rom_size * 16 * 1024);
-    printf("chr_rom: %u (%u)\n", h.chr_rom_size, h.chr_rom_size * 8 * 1024);
-    printf("flags_6: %u\n", h.flags_6);
-    printf("flags_7: %u\n", h.flags_7);
-    printf("prg_ram_size: %u (%u)\n",
-            h.prg_ram_size,
-            h.prg_ram_size * 8 * 1024);
-    printf("flags_9: %u\n", h.flags_9);
-    printf("flags_10: %u\n", h.flags_10);
-
     uint8_t mapper = (h.flags_6 & 0xF0) >> 4;
     mapper |= h.flags_7 & 0xF0;
-    printf("mapper: %u\n", mapper);
 
     const uint32_t expected_rom_size = sizeof(INesHeader) +
                                        h.prg_rom_size * 16 * 1024 +
@@ -83,13 +69,11 @@ IRom *RomFactory::fromFile(const std::string &filepath) {
 
     std::vector<uint8_t> prg_rom(bytes.begin() + 16,
             bytes.begin() + 16 + h.prg_rom_size * 16 * 1024);
-    printf("prg rom size: %zu\n", prg_rom.size());
 
     std::vector<uint8_t> chr_rom(
             bytes.begin() + 16 + h.prg_rom_size * 16 * 1024,
             bytes.begin() + 16 + h.prg_rom_size * 16 * 1024 +
                     h.chr_rom_size * 8 * 1024);
-    printf("chr rom size: %zu\n", chr_rom.size());
 
     if (mapper == 0) {
         return new Nrom(h, prg_rom, chr_rom);
