9c80eb3494615b0b61235193858cc4f1dd92ac34
Robin Lindén <dev@robinlinden.eu>
2019-01-26 18:10:01 +0100

    Set up compiler warnings in separate script

---
+++ flags.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+add_flag() { CXXFLAGS="$CXXFLAGS $@"; }
+
+add_flag -Wall
+add_flag -Werror
+add_flag -Wextra
+add_flag -Wswitch-enum
+add_flag -Wshadow
+add_flag -Wnon-virtual-dtor
+add_flag -pedantic-errors
+
+export CXXFLAGS=$CXXFLAGS
