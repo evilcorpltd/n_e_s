33de33c2508da8eb0ddb1e065b92db5ba7113d6d
Johan Norberg <norberg_johan@hotmail.com>
2019-10-02 19:35:42 -0500

    Add access methods to Nes

--- nes/include/nes/nes.h
+++ nes/include/nes/nes.h
@@ -3,6 +3,16 @@
 #include <memory>
 #include <string>
 
+namespace n_e_s::core {
+class ICpu;
+struct CpuRegisters;
+
+class IPpu;
+struct PpuRegisters;
+
+class IMmu;
+} // namespace n_e_s::core
+
 namespace n_e_s::nes {
 
 class Nes {
@@ -13,6 +23,26 @@ public:
     void execute();
     void reset();
     void load_rom(const std::string &filepath);
+
+    n_e_s::core::ICpu &cpu();
+    const n_e_s::core::ICpu &cpu() const;
+
+    n_e_s::core::IPpu &ppu();
+    const n_e_s::core::IPpu &ppu() const;
+
+    n_e_s::core::IMmu &mmu();
+    const n_e_s::core::IMmu &mmu() const;
+
+    n_e_s::core::IMmu &ppu_mmu();
+    const n_e_s::core::IMmu &ppu_mmu() const;
+
+    n_e_s::core::CpuRegisters &cpu_registers();
+    const n_e_s::core::CpuRegisters &cpu_registers() const;
+
+    n_e_s::core::PpuRegisters &ppu_registers();
+    const n_e_s::core::PpuRegisters &ppu_registers() const;
+
+    uint64_t current_cycle() const;
 
 private:
     class Impl;
