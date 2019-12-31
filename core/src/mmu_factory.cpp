1d9cf9992453d6d7fbbbf45da95dbfac9fef6aae
Robin Linden <dev@robinlinden.eu>
2019-12-31 17:00:25 +0100

    Fix compilation with gcc9+

--- core/src/mmu_factory.cpp
+++ core/src/mmu_factory.cpp
@@ -6,13 +6,14 @@
 namespace n_e_s::core {
 
 std::unique_ptr<IMmu> MmuFactory::create(MemBankList mem_banks) {
-    auto mmu = std::make_unique<Mmu>();
+    std::unique_ptr<IMmu> immu = std::make_unique<Mmu>();
+    auto mmu = static_cast<Mmu *>(immu.get());
 
     for (std::unique_ptr<IMemBank> &mem_bank : mem_banks) {
         mmu->add_mem_bank(std::move(mem_bank));
     }
 
-    return std::move(mmu);
+    return immu;
 }
 
 } // namespace n_e_s::core
