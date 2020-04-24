9539c2a100ab76ba6fa18b18fb8ad1d839bf577c
Johan Norberg <norberg_johan@hotmail.com>
2020-04-24 17:20:55 -0500

    Create memory for all namtables on Ppu
    * The mirrored memory for nametables is not the same size as the main
      memory. Had to remove condition that the address range is evenly
      dividable by the size for membanks.

--- core/src/membank_factory.cpp
+++ core/src/membank_factory.cpp
@@ -47,8 +47,9 @@ MemBankList MemBankFactory::create_nes_ppu_mem_banks() {
     // $2800-$2BFF  $0400   Nametable 2
     // $2C00-$2FFF  $0400   Nametable 3
     // $3000-$3EFF  $0F00   Mirrors of $2000-$2EFF
-    // TODO(JN), fix mirroring at 0x3000 - 0x3EFF
-    mem_banks.push_back(std::make_unique<MemBank<0x2000, 0x2FFF, 0x0400>>());
+    mem_banks.push_back(std::make_unique<MemBank<0x2000, 0x3EFF, 0x1000>>());
+
+    // Palettes
     mem_banks.push_back(std::make_unique<MemBank<0x3F00, 0x3FFF, 0x20>>());
 
     return mem_banks;
--- core/test/src/test_ppu_membank.cpp
+++ core/test/src/test_ppu_membank.cpp
@@ -16,6 +16,34 @@ public:
     std::unique_ptr<IMmu> mmu;
 };
 
+// Nametables
+TEST_F(PpuMembankTest, the_nametable_memory_works) {
+    mmu->write_byte(0x2000, 0x42);
+    EXPECT_EQ(0x42, mmu->read_byte(0x2000));
+    mmu->write_byte(0x2FFF, 0x39);
+    EXPECT_EQ(0x39, mmu->read_byte(0x2FFF));
+    EXPECT_EQ(0x42, mmu->read_byte(0x2000));
+}
+
+TEST_F(PpuMembankTest, the_nametable_memory_is_mirrored) {
+    const auto nametable_size = 0x400 * 4;
+    mmu->write_byte(0x2000, 0x01);
+    for (uint16_t addr = 0x2000; addr < 0x3EFF; addr += nametable_size) {
+        EXPECT_EQ(0x01, mmu->read_byte(addr));
+    }
+
+    mmu->write_byte(0x2EFA, 0x07);
+    for (uint16_t addr = 0x2EFA; addr < 0x3EFF; addr += nametable_size) {
+        EXPECT_EQ(0x07, mmu->read_byte(addr));
+    }
+
+    // Mirror of nametable 3 is smaller than the size of a nametable,
+    // so the memory between 0x3F00 and 0x3FFF should not be mirrored.
+    for (uint16_t addr = 0x3F00; addr < 0x4000; ++addr) {
+        EXPECT_EQ(0x00, mmu->read_byte(addr));
+    }
+}
+
 // Palette memory
 TEST_F(PpuMembankTest, the_palette_memory_works) {
     mmu->write_byte(0x3F00, 0x42);
