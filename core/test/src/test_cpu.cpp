e3723cb88f49cc34de843499e6933edbdc785b06
Robin Linden <dev@robinlinden.eu>
2020-04-24 21:03:11 +0200

    Add a test fixture for move tests

--- core/test/src/test_cpu.cpp
+++ core/test/src/test_cpu.cpp
@@ -243,61 +243,6 @@ public:
         }
     }
 
-    // This is a bit silly, but we need both registers.from and expected.from.
-    void move_test(uint8_t *expected_dest,
-            uint8_t *expected_from,
-            uint8_t *registers_from) {
-        *expected_from = *registers_from = 10;
-        *expected_dest = 10;
-
-        step_execution(2);
-        EXPECT_EQ(expected, registers);
-    }
-
-    void move_test_sets_n(uint8_t *expected_dest,
-            uint8_t *expected_from,
-            uint8_t *registers_from) {
-        *expected_from = *registers_from = 128;
-        *expected_dest = 128;
-        expected.p |= N_FLAG;
-
-        step_execution(2);
-        EXPECT_EQ(expected, registers);
-    }
-
-    void move_test_clears_n(uint8_t *expected_dest,
-            uint8_t *expected_from,
-            uint8_t *registers_from) {
-        *expected_from = *registers_from = 5;
-        *expected_dest = 5;
-        registers.p |= N_FLAG;
-
-        step_execution(2);
-        EXPECT_EQ(expected, registers);
-    }
-
-    void move_test_sets_z(uint8_t *expected_dest,
-            uint8_t *expected_from,
-            uint8_t *registers_from) {
-        *expected_from = *registers_from = 0;
-        *expected_dest = 0;
-        expected.p |= Z_FLAG;
-
-        step_execution(2);
-        EXPECT_EQ(expected, registers);
-    }
-
-    void move_test_clears_z(uint8_t *expected_dest,
-            uint8_t *expected_from,
-            uint8_t *registers_from) {
-        *expected_from = *registers_from = 5;
-        *expected_dest = 5;
-        registers.p |= Z_FLAG;
-
-        step_execution(2);
-        EXPECT_EQ(expected, registers);
-    }
-
     void load_immediate_sets_reg(uint8_t *reg) {
         *reg = 42;
         ++expected.pc;
@@ -496,6 +441,64 @@ public:
     CpuRegisters expected;
 
     InSequence in_sequence{};
+};
+
+class CpuMoveTest : public CpuTest {
+public:
+    // This is a bit silly, but we need both registers.from and expected.from.
+    void move_test(uint8_t *expected_dest,
+            uint8_t *expected_from,
+            uint8_t *registers_from) {
+        *expected_from = *registers_from = 10;
+        *expected_dest = 10;
+
+        step_execution(2);
+        EXPECT_EQ(expected, registers);
+    }
+
+    void move_test_sets_n(uint8_t *expected_dest,
+            uint8_t *expected_from,
+            uint8_t *registers_from) {
+        *expected_from = *registers_from = 128;
+        *expected_dest = 128;
+        expected.p |= N_FLAG;
+
+        step_execution(2);
+        EXPECT_EQ(expected, registers);
+    }
+
+    void move_test_clears_n(uint8_t *expected_dest,
+            uint8_t *expected_from,
+            uint8_t *registers_from) {
+        *expected_from = *registers_from = 5;
+        *expected_dest = 5;
+        registers.p |= N_FLAG;
+
+        step_execution(2);
+        EXPECT_EQ(expected, registers);
+    }
+
+    void move_test_sets_z(uint8_t *expected_dest,
+            uint8_t *expected_from,
+            uint8_t *registers_from) {
+        *expected_from = *registers_from = 0;
+        *expected_dest = 0;
+        expected.p |= Z_FLAG;
+
+        step_execution(2);
+        EXPECT_EQ(expected, registers);
+    }
+
+    void move_test_clears_z(uint8_t *expected_dest,
+            uint8_t *expected_from,
+            uint8_t *registers_from) {
+        *expected_from = *registers_from = 5;
+        *expected_dest = 5;
+        registers.p |= Z_FLAG;
+
+        step_execution(2);
+        EXPECT_EQ(expected, registers);
+    }
 };
 
 class CpuBranchTest : public CpuTest {
@@ -3136,111 +3139,111 @@ TEST_F(CpuTest, sax_indexed_indirect) {
 }
 
 // TSX
-TEST_F(CpuTest, tsx) {
+TEST_F(CpuMoveTest, tsx) {
     stage_instruction(TSX);
     move_test(&expected.x, &expected.sp, &registers.sp);
 }
-TEST_F(CpuTest, tsx_sets_n_flag) {
+TEST_F(CpuMoveTest, tsx_sets_n_flag) {
     stage_instruction(TSX);
     move_test_sets_n(&expected.x, &expected.sp, &registers.sp);
 }
-TEST_F(CpuTest, tsx_clears_n_flag) {
+TEST_F(CpuMoveTest, tsx_clears_n_flag) {
     stage_instruction(TSX);
     move_test_clears_n(&expected.x, &expected.sp, &registers.sp);
 }
-TEST_F(CpuTest, tsx_sets_z_flag) {
+TEST_F(CpuMoveTest, tsx_sets_z_flag) {
     stage_instruction(TSX);
     move_test_sets_z(&expected.x, &expected.sp, &registers.sp);
 }
-TEST_F(CpuTest, tsx_clears_z_flag) {
+TEST_F(CpuMoveTest, tsx_clears_z_flag) {
     stage_instruction(TSX);
     move_test_clears_z(&expected.x, &expected.sp, &registers.sp);
 }
 
 // TYA
-TEST_F(CpuTest, tya) {
+TEST_F(CpuMoveTest, tya) {
     stage_instruction(TYA);
     move_test(&expected.a, &expected.y, &registers.y);
 }
-TEST_F(CpuTest, tya_sets_n_flag) {
+TEST_F(CpuMoveTest, tya_sets_n_flag) {
     stage_instruction(TYA);
     move_test_sets_n(&expected.a, &expected.y, &registers.y);
 }
-TEST_F(CpuTest, tya_clears_n_flag) {
+TEST_F(CpuMoveTest, tya_clears_n_flag) {
     stage_instruction(TYA);
     move_test_clears_n(&expected.a, &expected.y, &registers.y);
 }
-TEST_F(CpuTest, tya_sets_z_flag) {
+TEST_F(CpuMoveTest, tya_sets_z_flag) {
     stage_instruction(TYA);
     move_test_sets_z(&expected.a, &expected.y, &registers.y);
 }
-TEST_F(CpuTest, tya_clears_z_flag) {
+TEST_F(CpuMoveTest, tya_clears_z_flag) {
     stage_instruction(TYA);
     move_test_clears_z(&expected.a, &expected.y, &registers.y);
 }
 
 // TAX
-TEST_F(CpuTest, tax) {
+TEST_F(CpuMoveTest, tax) {
     stage_instruction(TAX);
     move_test(&expected.x, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tax_sets_n_flag) {
+TEST_F(CpuMoveTest, tax_sets_n_flag) {
     stage_instruction(TAX);
     move_test_sets_n(&expected.x, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tax_clears_n_flag) {
+TEST_F(CpuMoveTest, tax_clears_n_flag) {
     stage_instruction(TAX);
     move_test_clears_n(&expected.x, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tax_sets_z_flag) {
+TEST_F(CpuMoveTest, tax_sets_z_flag) {
     stage_instruction(TAX);
     move_test_sets_z(&expected.x, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tax_clears_z_flag) {
+TEST_F(CpuMoveTest, tax_clears_z_flag) {
     stage_instruction(TAX);
     move_test_clears_z(&expected.x, &expected.a, &registers.a);
 }
 
 // TAY
-TEST_F(CpuTest, tay) {
+TEST_F(CpuMoveTest, tay) {
     stage_instruction(TAY);
     move_test(&expected.y, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tay_sets_n_flag) {
+TEST_F(CpuMoveTest, tay_sets_n_flag) {
     stage_instruction(TAY);
     move_test_sets_n(&expected.y, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tay_clears_n_flag) {
+TEST_F(CpuMoveTest, tay_clears_n_flag) {
     stage_instruction(TAY);
     move_test_clears_n(&expected.y, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tay_sets_z_flag) {
+TEST_F(CpuMoveTest, tay_sets_z_flag) {
     stage_instruction(TAY);
     move_test_sets_z(&expected.y, &expected.a, &registers.a);
 }
-TEST_F(CpuTest, tay_clears_z_flag) {
+TEST_F(CpuMoveTest, tay_clears_z_flag) {
     stage_instruction(TAY);
     move_test_clears_z(&expected.y, &expected.a, &registers.a);
 }
 
 // TXA
-TEST_F(CpuTest, txa) {
+TEST_F(CpuMoveTest, txa) {
     stage_instruction(TXA);
     move_test(&expected.a, &expected.x, &registers.x);
 }
-TEST_F(CpuTest, txa_sets_n_flag) {
+TEST_F(CpuMoveTest, txa_sets_n_flag) {
     stage_instruction(TXA);
     move_test_sets_n(&expected.a, &expected.x, &registers.x);
 }
-TEST_F(CpuTest, txa_clears_n_flag) {
+TEST_F(CpuMoveTest, txa_clears_n_flag) {
     stage_instruction(TXA);
     move_test_clears_n(&expected.a, &expected.x, &registers.x);
 }
-TEST_F(CpuTest, txa_sets_z_flag) {
+TEST_F(CpuMoveTest, txa_sets_z_flag) {
     stage_instruction(TXA);
     move_test_sets_z(&expected.a, &expected.x, &registers.x);
 }
-TEST_F(CpuTest, txa_clears_z_flag) {
+TEST_F(CpuMoveTest, txa_clears_z_flag) {
     stage_instruction(TXA);
     move_test_clears_z(&expected.a, &expected.x, &registers.x);
 }
