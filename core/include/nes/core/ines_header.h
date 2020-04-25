30dbefdf55526bf976c8155d7c9f585faef7c668
Johan Norberg <norberg_johan@hotmail.com>
2020-04-25 09:41:40 -0500

    Move INesHeader to new file

---
+++ core/include/nes/core/ines_header.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+namespace n_e_s::core {
+
+struct INesHeader {
+    uint8_t nes[4]{'N', 'E', 'S', 26}; // NES + MS-DOS EOF
+    uint8_t prg_rom_size; // in 16 KB units.
+    uint8_t chr_rom_size; // in 8 KB units.
+    uint8_t flags_6;
+    uint8_t flags_7;
+    uint8_t prg_ram_size; // in 8 KB units.
+    uint8_t flags_9;
+    uint8_t flags_10;
+    uint8_t zeros[5]{0};
+};
+
+} // namespace n_e_s::core
