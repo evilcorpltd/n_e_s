3af4f4b19ef0bc21afd8dd719ab78bd873f173d3
Johan Norberg <norberg_johan@hotmail.com>
2022-03-19 15:49:38 +0100

    Allow test roms with different mappers

--- core/test/src/test_rom.cpp
+++ core/test/src/test_rom.cpp
@@ -11,14 +11,19 @@ using namespace n_e_s::core;
 
 namespace {
 
-std::string ines_header_bytes(const uint8_t mapper,
+enum class Mapper {
+    Nrom = 0,
+};
+
+std::string ines_header_bytes(const uint8_t mapper_id,
         const uint8_t prg_rom_size,
         const uint8_t chr_rom_size,
         const uint8_t prg_ram_size) {
     INesHeader header{};
-    header.flags_6 = static_cast<uint8_t>(mapper & static_cast<uint8_t>(0x0Fu))
-                     << 4u;
-    header.flags_7 = static_cast<uint8_t>(mapper & static_cast<uint8_t>(0xF0u));
+    header.flags_6 =
+            static_cast<uint8_t>(mapper_id & static_cast<uint8_t>(0x0Fu)) << 4u;
+    header.flags_7 =
+            static_cast<uint8_t>(mapper_id & static_cast<uint8_t>(0xF0u));
     header.prg_rom_size = prg_rom_size;
     header.chr_rom_size = chr_rom_size;
     header.prg_ram_size = prg_ram_size;
@@ -32,8 +37,11 @@ std::string ines_header_bytes(const uint8_t mapper,
     return bytes;
 }
 
-std::string nrom_bytes(const uint8_t prg_rom_size, const uint8_t chr_rom_size) {
-    std::string bytes{ines_header_bytes(0, prg_rom_size, chr_rom_size, 0)};
+std::string nrom_bytes(const uint8_t prg_rom_size,
+        const uint8_t chr_rom_size,
+        const Mapper mapper) {
+    std::string bytes{ines_header_bytes(
+            static_cast<uint8_t>(mapper), prg_rom_size, chr_rom_size, 0)};
 
     bytes.append(prg_rom_size * 16 * 1024, 0);
     bytes.append(chr_rom_size * 8 * 1024, 0);
@@ -63,19 +71,19 @@ TEST(RomFactory, fails_if_mapper_not_supported) {
 }
 
 TEST(Nrom, creation_works_with_correct_rom_sizes) {
-    std::string bytes{nrom_bytes(1, 1)};
+    std::string bytes{nrom_bytes(1, 1, Mapper::Nrom)};
     std::stringstream ss(bytes);
     std::unique_ptr<IRom> nrom{RomFactory::from_bytes(ss)};
     EXPECT_NE(nullptr, nrom);
 
-    bytes = nrom_bytes(2, 1);
+    bytes = nrom_bytes(2, 1, Mapper::Nrom);
     ss = std::stringstream(bytes);
     nrom = RomFactory::from_bytes(ss);
     EXPECT_NE(nullptr, nrom);
 }
 
 TEST(Nrom, has_the_correct_cpu_address_space) {
-    std::string bytes{nrom_bytes(1, 1)};
+    std::string bytes{nrom_bytes(1, 1, Mapper::Nrom)};
     std::stringstream ss(bytes);
     std::unique_ptr<IRom> nrom{RomFactory::from_bytes(ss)};
     EXPECT_TRUE(nrom->is_cpu_address_in_range(0x6000));
@@ -83,7 +91,7 @@ TEST(Nrom, has_the_correct_cpu_address_space) {
 }
 
 TEST(Nrom, has_the_correct_ppu_address_space) {
-    std::string bytes{nrom_bytes(1, 1)};
+    std::string bytes{nrom_bytes(1, 1, Mapper::Nrom)};
     std::stringstream ss(bytes);
     std::unique_ptr<IRom> nrom{RomFactory::from_bytes(ss)};
     EXPECT_TRUE(nrom->is_ppu_address_in_range(0x0000));
@@ -91,30 +99,30 @@ TEST(Nrom, has_the_correct_ppu_address_space) {
 }
 
 TEST(Nrom, creation_fails_with_bad_rom_sizes) {
-    std::string bytes{nrom_bytes(0, 1)};
+    std::string bytes{nrom_bytes(0, 1, Mapper::Nrom)};
     std::stringstream ss(bytes);
     EXPECT_THROW(auto tmp = RomFactory::from_bytes(ss), std::invalid_argument);
 
-    bytes = nrom_bytes(1, 1);
+    bytes = nrom_bytes(1, 1, Mapper::Nrom);
     bytes.pop_back();
     ss = std::stringstream(bytes);
     EXPECT_THROW(auto tmp = RomFactory::from_bytes(ss), std::invalid_argument);
 
-    bytes = nrom_bytes(3, 1);
+    bytes = nrom_bytes(3, 1, Mapper::Nrom);
     ss = std::stringstream(bytes);
     EXPECT_THROW(auto tmp = RomFactory::from_bytes(ss), std::invalid_argument);
 
-    bytes = nrom_bytes(1, 0);
+    bytes = nrom_bytes(1, 0, Mapper::Nrom);
     ss = std::stringstream(bytes);
     EXPECT_THROW(auto tmp = RomFactory::from_bytes(ss), std::invalid_argument);
 
-    bytes = nrom_bytes(1, 2);
+    bytes = nrom_bytes(1, 2, Mapper::Nrom);
     ss = std::stringstream(bytes);
     EXPECT_THROW(auto tmp = RomFactory::from_bytes(ss), std::invalid_argument);
 }
 
 TEST(Nrom, write_and_read_byte_ppu_bus) {
-    std::string bytes{nrom_bytes(1, 1)};
+    std::string bytes{nrom_bytes(1, 1, Mapper::Nrom)};
     std::stringstream ss(bytes);
     std::unique_ptr<IRom> nrom = RomFactory::from_bytes(ss);
 
@@ -123,7 +131,7 @@ TEST(Nrom, write_and_read_byte_ppu_bus) {
 }
 
 TEST(Nrom, write_and_read_byte_cpu_bus) {
-    std::string bytes{nrom_bytes(1, 1)};
+    std::string bytes{nrom_bytes(1, 1, Mapper::Nrom)};
     std::stringstream ss(bytes);
     std::unique_ptr<IRom> nrom = RomFactory::from_bytes(ss);
 
