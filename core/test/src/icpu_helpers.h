ec2c64110118fe6b53f906c9f0b37ba3c582064b
Johan Norberg <norberg_johan@hotmail.com>
2022-03-26 16:06:38 +0100

    Use compiler generated comparison operator for cpu registers

--- core/test/src/icpu_helpers.h
+++ core/test/src/icpu_helpers.h
@@ -6,8 +6,6 @@
 
 namespace n_e_s::core {
 
-bool operator==(const CpuRegisters &a, const CpuRegisters &b);
-
 // Required by gtest to use pascal case.
 // NOLINTNEXTLINE(readability-identifier-naming)
 void PrintTo(const CpuRegisters &r, std::ostream *os);
