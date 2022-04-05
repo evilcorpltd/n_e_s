1a1c727fe36da2c93cdf468c86ae1653eca6a38b
Johan Norberg <norberg_johan@hotmail.com>
2022-04-05 22:21:01 +0200

    Update Pixel to use a Color struct for pixel color
    * Makes it easier to understand what color is.

--- core/include/nes/core/pixel.h
+++ core/include/nes/core/pixel.h
@@ -4,10 +4,16 @@
 
 namespace n_e_s::core {
 
+struct Color {
+    uint8_t r{0u};
+    uint8_t g{0u};
+    uint8_t b{0u};
+};
+
 struct Pixel {
-    uint8_t x;
-    uint8_t y;
-    uint32_t color;
+    uint8_t x{0u};
+    uint8_t y{0u};
+    Color color;
 };
 
 } // namespace n_e_s::core
